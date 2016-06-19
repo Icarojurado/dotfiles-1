4e38408bb1dca20a1aab55f205c9e593b24aec9f
Gustavo Felizola <gfelizola@gmail.com>
2016-06-19 11:31:52 -0300

    applications to install

--- src/os/install/macos/misc.sh
+++ src/os/install/macos/misc.sh
@@ -6,13 +6,13 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-brew_install "Android File Transfer" "android-file-transfer" "caskroom/cask" "cask"
+# brew_install "Android File Transfer" "android-file-transfer" "caskroom/cask" "cask"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 brew_install "Chrome" "google-chrome" "caskroom/cask" "cask"
-brew_install "Chrome Canary" "google-chrome-canary" "caskroom/versions" "cask"
-brew_install "Chromium" "chromium" "caskroom/cask" "cask"
+# brew_install "Chrome Canary" "google-chrome-canary" "caskroom/versions" "cask"
+# brew_install "Chromium" "chromium" "caskroom/cask" "cask"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -22,11 +22,11 @@ brew_install "FFmpeg" "ffmpeg"
 
 brew_install "Firefox" "firefox" "caskroom/cask" "cask"
 brew_install "Firefox Developer" "firefoxdeveloperedition" "caskroom/versions" "cask"
-brew_install "Firefox Nightly" "firefoxnightly" "caskroom/versions" "cask"
+# brew_install "Firefox Nightly" "firefoxnightly" "caskroom/versions" "cask"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-brew_install "Flash" "flash" "caskroom/cask" "cask"
+# brew_install "Flash" "flash" "caskroom/cask" "cask"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -50,17 +50,17 @@ brew_install "ImageOptim" "imageoptim" "caskroom/cask" "cask"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-brew_install "LibreOffice" "libreoffice" "caskroom/cask" "cask"
+# brew_install "LibreOffice" "libreoffice" "caskroom/cask" "cask"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-brew_install "LICEcap" "licecap" "caskroom/cask" "cask"
+# brew_install "LICEcap" "licecap" "caskroom/cask" "cask"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-brew_install "Opera" "opera" "caskroom/cask" "cask"
-brew_install "Opera Beta" "opera-beta" "caskroom/versions" "cask"
-brew_install "Opera Developer" "opera-developer" "caskroom/versions" "cask"
+# brew_install "Opera" "opera" "caskroom/cask" "cask"
+# brew_install "Opera Beta" "opera-beta" "caskroom/versions" "cask"
+# brew_install "Opera Developer" "opera-developer" "caskroom/versions" "cask"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -115,4 +115,4 @@ brew_install "WebKit" "webkit-nightly" "caskroom/versions" "cask"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-brew_install "Zopfli" "zopfli"
+# brew_install "Zopfli" "zopfli"
